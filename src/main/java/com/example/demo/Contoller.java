4cee7808c43fcd35004ab800b5cdac3a895686be
Manjunatha Patkar <manjunathapatkar150@gmail.com>
2020-12-29 10:25:55 +0530

    Fixed bugs now works properly

--- src/main/java/com/example/demo/Contoller.java
+++ src/main/java/com/example/demo/Contoller.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Optional;
@@ -22,10 +23,11 @@ public class Contoller {
         return "home.jsp";
      }
      @RequestMapping("getAlien")
-    public ModelAndView getAlien(int aid){
+    public ModelAndView getAlien(@RequestParam int aid){
         ModelAndView mv=new ModelAndView();
-        mv.setViewName("showdetails.jps");
-        Optional<Alien> alien=alienInterface.findById(aid);
+        mv.setViewName("showdetails.jsp");
+        Alien alien=alienInterface.findById(aid).orElse(new Alien());
+//        Optional<Alien> alien=alienInterface.findById(aid);
         mv.addObject(alien);
         return mv;
      }
